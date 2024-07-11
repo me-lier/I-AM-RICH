cbd2026e8430f11c2f1545183494547b519914e8
Srinivas Deekonda <126309422+me-lier@users.noreply.github.com>
2024-07-11 15:49:34 +0530

    Add files via upload

---
+++ I AM RICH/I AM RICH/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I AM RICH
+//
+//  Created by Srinivas Deekonda on 11/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
